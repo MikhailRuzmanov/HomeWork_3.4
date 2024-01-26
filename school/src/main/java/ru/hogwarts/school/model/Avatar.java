91f34962fd4d4282c78a604246c5a5cb3418d443
MikhailRuzmanov <mihailruzmanov@mail.ru>
2024-01-26 04:25:23 +0300

    Домашняя работа 3.4

---
+++ school/src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.school.model;
+
+import jakarta.persistence.*;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity
+public class Avatar {
+
+}
---
+++ school/src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -0,0 +1,25 @@
+package ru.hogwarts.school.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositories.AvatarRepository;
+import ru.hogwarts.school.repositories.StudentRepository;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+@Service
+@Transactional
+public class AvatarService {
+
+}
