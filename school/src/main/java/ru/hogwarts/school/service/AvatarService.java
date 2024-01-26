cd3d5f881318f3636b575c73b311ba3f47218da9
MikhailRuzmanov <mihailruzmanov@mail.ru>
2024-01-26 04:27:22 +0300

    Домашняя работа 3.4 Готово

--- school/src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ school/src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -21,5 +21,47 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 @Service
 @Transactional
 public class AvatarService {
+    @Value("${path.to.avatars.folder}")
+    private String avatarsDir;
+    private  final StudentService studentService;
+    private final AvatarRepository avatarRepository;
+    private final StudentRepository studentRepository;
 
+    public AvatarService(StudentService studentService, AvatarRepository avatarRepository, StudentRepository studentRepository) {
+        this.studentService = studentService;
+        this.avatarRepository = avatarRepository;
+        this.studentRepository = studentRepository;
+    }
+
+
+    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        Student student = studentRepository.getById(studentId);
+        Path filePath = Path.of(avatarsDir, student + "." + getExtensions(avatarFile.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try (
+                InputStream is = avatarFile.getInputStream();
+                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+        ) {
+            bis.transferTo(bos);
+        }
+        Avatar avatar = findAvatar(studentId);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(avatarFile.getBytes());
+        avatarRepository.save(avatar);
+    }
+    private String getExtensions(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+    public Avatar findAvatar(Long student_id) {
+        if (avatarRepository.findByStudentId(student_id) != null) {
+            return avatarRepository.findByStudentId(student_id);
+        }
+        return new Avatar();
+    }
 }
